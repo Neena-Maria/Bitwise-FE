f7e08d939169683c2bea352a5b3eb7d8f1400379
Neena-Maria <neena@keyvalue.systems>
2023-09-16 00:32:58 +0530

    fix: sidebar navigation

--- src/components/sidebar/SideBar.config.ts
+++ src/components/sidebar/SideBar.config.ts
@@ -1,22 +1,18 @@
 export type SideNavItem = {
   id: string;
   name: string;
-  path: string;
 };
 export const sideNavItems: SideNavItem[] = [
   {
     id: "myDocs",
     name: "Text Editor",
-    path: "/documents",
   },
   {
     id: "docs",
     name: "Docs",
-    path: "/google-docs",
   },
   {
     id: "board",
     name: "Task Board",
-    path: "/board",
   },
 ];
