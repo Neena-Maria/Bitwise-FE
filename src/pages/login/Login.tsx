6ed77af85eaa03d1b5fa330f4c85d0eb27c75e00
Meenu-Iswar <meenu@keyvalue.systems>
2023-09-15 13:42:03 +0530

    added logo

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -25,12 +25,8 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-repeat bg-[#f5f5f5]">
       <div className="flex flex-col relative items-center w-[560px] h-[300px] bg-white rounded-lg shadow-[0_2px_10px_rgba(0,0,0,0.1)]">
-        <GoogleLogo height={100} className="absolute " />
-        <div className="mt-20 w-full text-[35px] font-medium text-center">
-          Sign In
-        </div>
         <button
-          className="flex items-center pl-[8px] mt-[64px] w-[245px] h-[56px] align-middle bg-[#6216fd] rounded-[54px]"
+          className="flex items-center pl-[8px] mt-[64px] w-[245px] h-[56px] align-middle bg-blue-500 rounded-[54px]"
           onClick={onClickSignin}
           type="button"
         >
